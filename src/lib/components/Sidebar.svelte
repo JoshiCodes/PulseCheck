f49c132ceebb9f1a04dc3e9b384422834455195b
JoshiCodes <55353244+JoshiCodes@users.noreply.github.com>
2026-07-20 13:33:42 +0200

    Added the Notification Provider into the Sidebar

--- src/lib/components/Sidebar.svelte
+++ src/lib/components/Sidebar.svelte
@@ -5,12 +5,12 @@
         { name: 'Settings', href: '/admin' },
         { name: 'Monitors', href: '/admin/monitors' },
         { name: 'Pages', href: '/admin/pages' },
-        { name: 'User Management', href: '/admin/users' },
+        { name: 'User Management', href: '/admin/users', disabled: true },
         { name: 'OIDC Providers', href: '/admin/oidc', },
+        { name: 'Notification Providers', href: '/admin/notifications' },
         { name: 'Custom Links', href: '/admin/links', }
     ];
 
-    // Hilfsfunktion um zu prüfen, ob der Pfad aktiv ist
     const isActive = (href: string) => {
         if (href === '/admin') return page.url.pathname === '/admin';
         return page.url.pathname.startsWith(href);
@@ -23,11 +23,13 @@
         {#each menuItems as item}
             {@const active = isActive(item.href)}
             <a
-                    href={item.href}
+                    href={item.disabled ? "#" : item.href}
                     class="flex items-center gap-3 rounded-lg px-3 py-2 text-sm font-medium transition-colors duration-200
 				{active
 					? 'bg-primary-200 dark:bg-primary-950 text-primary-700 dark:text-primary-400'
-					: 'text-zinc-600 hover:bg-zinc-100 hover:text-zinc-900 dark:text-zinc-400 dark:hover:bg-zinc-900 dark:hover:text-zinc-100'}"
+					: item.disabled ? 'text-zinc-400 dark:text-zinc-500' : 'text-zinc-600 hover:bg-zinc-100 hover:text-zinc-900 dark:text-zinc-400 dark:hover:bg-zinc-900 dark:hover:text-zinc-100'}
+
+            "
             >
                 {item.name}
             </a>
