5d72146102d5d6abeeb71c333e88002cfb53bb77
JoshiCodes <55353244+JoshiCodes@users.noreply.github.com>
2026-07-20 15:37:33 +0200

    Added the first version of the configuration dashboard for notifications

---
+++ src/lib/components/NotificationProviderContainer.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+
+	import type { Snippet } from 'svelte';
+
+	type Props = {
+		title: string,
+		description?: string|null,
+		children: Snippet
+	}
+
+	let { title, description, children }: Props = $props();
+
+</script>
+
+
+<div class="">
+	<h3 class="text-2xl font-bold">{title}</h3>
+	{#if description}
+		<p class="text-zinc-500">
+			{description}
+		</p>
+	{/if}
+	<div class="mt-2 md:mt-4">
+		{@render children()}
+	</div>
+</div>
---
+++ src/routes/(protected)/admin/notifications/+page.svelte
@@ -0,0 +1,32 @@
+<script>
+import { goto } from '$app/navigation';
+import Badge from '$lib/components/Badge.svelte';
+import Button from '$lib/components/form/Button.svelte';
+import NotificationProviderContainer from '$lib/components/NotificationProviderContainer.svelte';
+import Checkbox from '$lib/components/form/Checkbox.svelte';
+import Input from '$lib/components/form/Input.svelte';
+
+const providers = [];
+
+</script>
+
+<div class="space-y-8">
+	<header>
+		<h1 class="text-3xl font-bold">Notification Providers</h1>
+		<p class="text-zinc-500">Manage how notifications reach you.</p>
+	</header>
+
+	<div class="">
+		<div class="mt-2 md:mt-4">
+			<NotificationProviderContainer title="Email (SMTP)">
+				<form action="?" method="post">
+					<Checkbox id="mail-enabled" name="mail-enabled" label="Enabled" />
+					<div class="mt-2 md:grid md:grid-cols-2 w-[50%]">
+						<Input id="mail-host" name="mail-host" label="SMTP Host" class="w-1/3" />
+						<Input id="mail-host" name="mail-host" label="SMTP Host" />
+					</div>
+				</form>
+			</NotificationProviderContainer>
+		</div>
+	</div>
+</div>
